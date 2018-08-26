5ebb59e15e0368ba44832086766f5de09e6981f6
Liza <lizalepage@gmail.com>
2018-08-26 15:49:13 -0400

    old tasks hidden

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -9,6 +9,8 @@ class TaskList extends Component {
 
   this.state = {
     tasks: [],
+    newTaskDescription: "",
+    newTaskPriority: ""
   };
 
 
@@ -24,6 +26,24 @@ class TaskList extends Component {
     });
   }
 
+  handleChange(e){
+    this.setState({newTaskDescription: e.target.value})
+  }
+
+  handleChangePriority(e){
+    this.setState({newTaskPriority: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.taskRef.push({
+      name: this.state.newTaskDescription,
+      priority: this.state.newTaskPriority,
+      status: "Active",
+      timecreated: new Date(),
+    });
+  }
+
   render(){
     return(
       <section className="task-box">
@@ -31,17 +51,34 @@ class TaskList extends Component {
         <section className = 'taskList'>
 
         {this.state.tasks.map( (task, index) => {
-          return <div key={task.key}>
-          <li>
-            <input type="checkbox" checked={task.status.complete} />
-            <span>{task.name} </span>
-          </li>
-          </div>
-      })
-    }
+          var d = new Date();
+          d.setDate(d.getDate() - 7)
+          if (d <= task.timecreated){
+
+            return <div key={task.key}>
+              <li>
+                <input type="checkbox" checked={task.status.complete} />
+                <span>{task.name} </span>
+              </li>
+            </div>
+
+
+      }
+    })
+  }
 
 
       </section>
+
+      <form onSubmit={ (e) => this.handleSubmit(e)}>
+        <input type="text" value={this.state.newTaskDescription} onChange={ (e) => this.handleChange(e)} />
+        <select id="priority" value={this.state.newTaskPriority} onChange={ (e) => this.handleChangePriority(e)}>
+          <option value="1"> Low </option>
+          <option value="2"> Medium </option>
+          <option value="3"> High </option>
+        </select>
+        <input type="submit"/>
+      </form>
     </section>
     )
   }
